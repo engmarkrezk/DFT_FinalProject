eb03e3bd1964139e11d223e29b1dbe3e85b79edb
Racheal Komuhendo <korabite@yahoo.com>
2020-08-26 18:51:28 -0400

    R work

--- DFT_Final_Project _Racheal.R
+++ DFT_Final_Project _Racheal.R
@@ -6,24 +6,44 @@ library("rcompanion")
 library("car")
 # correct for violation of homogeneity
 
-#Importing Dataset
-SFR <- covid19_homeless_impact_WR2.python
-
-# We will be running a One way Anova statistical analysis for the question of
-# Is there a difference homeless and the Settled population in terms of percentage cases of population?
-
-# Test assumptions 
-# 1. Normality for Homeless population percent cases and settled population percent cases
-
-plotNormalHistogram(SFR$total_homless_population_by_percentage )
-# Homeless population percent cases Look normally distributed
-
-plotNormalHistogram(SFR$total_population_by_percentage )
-# Settled population percent cases Look normally distributed
-
-# Testing for Homogeneity of variance since both are normally distributed
-bartlett.test(total_population_by_percentage ~ county ,data=SF)
-
-
 #One way anova
 prop.test(x = 246, n = 6423, alternative = "less")
+#Importing Dataset
+SFR <- covid19_homeless_impact_WR2_py
+
+# Linear Regression for QN: Are the number of homeless cases affecting the general public
+library("car")
+library("caret")
+library("gvlma")
+library("predictmeans")
+library("e1071")
+library("sandwich")
+# Testing Linearity
+scatter.smooth(x=SFR$homless_population_by_percentage, y=SFR$general_public_population_by_percentage, main="General Public cases vs Homeless cases")
+## Passed the assumption of linearity.
+
+#Testing for Homoscedasticity.
+### Creating a linear Model
+lmMod <- lm(general_public_population_by_percentage~homless_population_by_percentage, data=SFR)
+### Test for homoscedasticity
+par(mfrow=c(2,2))
+plot(lmMod)
+#### the left graphs both dont have flat red lines, hence Heteroscedasticity.
+
+#Further test for heteroscedasticity with Breush-Pagan test
+lmtest::bptest(lmMod)
+## pvalue= .01 < .05 hence proving heteroscedasticity.
+
+# correcting heteroscedasticity
+distBCMod1 <- caret::BoxCoxTrans(SFR$general_public_population_by_percentage)
+print(distBCMod1)
+## Binding to our dataset
+SFRR <- cbind(SFR, dist_newM=predict(distBCMod1, SFR$general_public_population_by_percentage))
+lmMod_bc2 <- lm(dist_newM~homless_population_by_percentage, data=SFRR)
+lmtest::bptest(lmMod_bc2)
+# the pvalue is still <.05 hence violating the assumption of homoscadasticity.
+
+# Testing for outliers
+CookD(lmMod, group=NULL, plot=TRUE, idn=3, newwd=TRUE)
+
+
